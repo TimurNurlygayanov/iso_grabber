36ddbb3002afa5790753812701433afc1c451b42
Viktor Ryzhenkin <vryzhenkin@mirantis.com>
2015-01-15 16:54:38 +0300

    Replaced print functions to logger

--- core.py
+++ core.py
@@ -1,29 +1,29 @@
-import libtorrent as lt
 import time
 import requests
 import ConfigParser
 import logging
-import traceback
+import libtorrent as lt
 from jenkinsapi.jenkins import Jenkins
 
 
 LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+fh = logging.FileHandler('grabber.log')
+LOG.addHandler(fh)
 
 
 class IsoSearchCore():
     def __init__(self):
-        try:
-            self.config = ConfigParser.RawConfigParser(allow_no_value=True)
-            self.config.read('config.conf')
-            self.fuel_version = self.config.get("jenkins", "fuel_version")
-            self.jenkins_url = self.config.get("jenkins", "url")
-            self.jenkins = Jenkins(self.jenkins_url, username=None,
-                                   password=None)
-            self.job_name = '{0}.all'.format(self.fuel_version)
-            self.job = self.jenkins[self.job_name]
-        except ConfigParser.ParsingError:
-            LOG.error(traceback.format_exc())
-        pass
+        self.config = ConfigParser.RawConfigParser(allow_no_value=True)
+        self.config.read('config.conf')
+        self.fuel_version = self.config.get("jenkins", "fuel_version")
+        self.jenkins_url = self.config.get("jenkins", "url")
+        self.jenkins = Jenkins(self.jenkins_url, username=None,
+                               password=None)
+        self.job_name = '{0}.all'.format(self.fuel_version)
+        self.job = self.jenkins[self.job_name]
+        self.stable_iso_number = self.find_correct_iso()
+        self.stable_iso_build = self.job.get_build(self.stable_iso_number)
 
     def get_downstream_job_list(self):
         return self.job.get_downstream_job_names()
@@ -31,26 +31,27 @@ class IsoSearchCore():
     def find_correct_iso(self):
         last_good_build = self.job.get_last_good_build()
         mark_stable = False
+        LOG.debug('Starting...')
         while mark_stable is False:
 
             last_good_build_number = last_good_build.get_number()
-            print 'Try build: {0}'.format(last_good_build)
-            print '------------------'
+            LOG.debug('Try build: {0}'.format(last_good_build))
+            LOG.debug('------------------')
             downstream_job_list = self.get_downstream_job_list()
-            print downstream_job_list
+            LOG.debug('Got downstream jobs: {0}'.format(downstream_job_list))
 
             for job in downstream_job_list:
                 last_job_build = self.jenkins[job].get_last_build()
-                print '******************'
-                print 'Checking downstream job {0}'.format(job)
+                LOG.debug('******************')
+                LOG.debug('Checking downstream job {0}'.format(job))
 
-                while last_job_build.get_upstream_build_number() \
-                        != last_good_build.get_number() or \
-                        last_job_build.get_upstream_job_name() != \
-                        self.job_name:
+                while (last_job_build.get_upstream_build_number()
+                       != last_good_build.get_number() or
+                       last_job_build.get_upstream_job_name() !=
+                       self.job_name):
 
                     if last_job_build.get_upstream_job_name() != self.job_name:
-                        print 'Skipped: {0}'.format(last_job_build)
+                        LOG.debug('Skipped: {0}'.format(last_job_build))
                         last_number = last_job_build.get_number() - 1
                         last_job_build = self.jenkins[job].get_build(
                             last_number)
@@ -59,22 +60,22 @@ class IsoSearchCore():
                         last_number = last_job_build.get_number() - 1
                         last_job_build = self.jenkins[job].get_build(
                             last_number)
-                        print 'Skipped: {0}'.format(last_job_build)
+                        LOG.debug('Skipped: {0}'.format(last_job_build))
 
-                    while last_job_build.get_upstream_job_name() == \
-                            self.job_name:
+                    while (last_job_build.get_upstream_job_name() ==
+                               self.job_name):
 
                         if last_job_build.get_upstream_job_name() is None:
                             last_number = last_job_build.get_number() - 1
                             last_job_build = self.jenkins[job].get_build(
                                 last_number)
-                            print 'Skipped: {0}'.format(last_job_build)
+                            LOG.debug('Skipped: {0}'.format(last_job_build))
                             break
 
-                        if last_job_build.get_upstream_build_number() == \
-                                last_good_build.get_number():
-                            print 'Found: {0} at {1}'.format(last_good_build,
-                                                             last_job_build)
+                        if (last_job_build.get_upstream_build_number() ==
+                                last_good_build.get_number()):
+                            LOG.debug('Found: {0} at {1}'
+                                      .format(last_good_build, last_job_build))
                             break
 
                         last_number = last_job_build.get_number() - 1
@@ -82,25 +83,25 @@ class IsoSearchCore():
                             last_number)
 
                 if last_job_build.get_status() == 'SUCCESS':
-                    print 'Test downstream job build passed'
+                    LOG.debug('Test downstream job build passed')
                     mark_stable = True
                 else:
-                    print 'Test downstream job build failed'
+                    LOG.warning('Test downstream job build failed')
                     next_build_number = last_good_build_number - 1
                     last_good_build = self.job.get_build(next_build_number)
                     while last_good_build.get_status() != 'SUCCESS':
                         next_build_number -= 1
                         last_good_build = self.job.get_build(next_build_number)
-                    print 'Now checking iso#{0}'.format(next_build_number)
+                    LOG.debug('Now checking iso#{0}'.format(next_build_number))
                     mark_stable = False
                     break
-        print 'Stable ISO found: {0}'.format(last_good_build)
+        LOG.debug('Stable ISO found: {0}'.format(last_good_build))
         return last_good_build.get_number()
 
     def get_magnet_link(self):
         magnet = requests.get('{0}view/{1}/job/{2}/{3}/artifact/magnet_link.txt'
                               .format(self.jenkins_url, self.fuel_version,
-                                      self.job_name, self.find_correct_iso()))
+                                      self.job_name, self.stable_iso_number))
         magnet = magnet.text.encode('ascii').strip().strip('MAGNET_LINK=')
         return magnet
 
@@ -124,22 +125,23 @@ class IsoGrabberCore(IsoSearchCore):
         handle = lt.add_magnet_uri(session, self.magnet_link, params)
         session.start_dht()
 
-        print 'Downloading metadata...'
+        LOG.debug(
+            'Downloading metadata for build {0}'.format(self.stable_iso_build))
         while not handle.has_metadata():
             time.sleep(1)
-        print 'Got metadata, starting torrent download...'
+        LOG.debug('Got metadata, starting torrent download...')
         while handle.status().state != lt.torrent_status.seeding:
             status = handle.status()
             state_str = ['queued', 'checking', 'downloading metadata',
                          'downloading',
                          'finished', 'seeding', 'allocating']
-            print '{0:.2f}% complete (down: {1:.1f} kb/s up: {2:.1f} kB/s ' \
-                  'peers: {3:d}) {4:s} {5:d}.3' \
-                .format(status.progress * 100,
-                        status.download_rate / 1000,
-                        status.upload_rate / 1000,
-                        status.num_peers,
-                        state_str[status.state],
-                        status.total_download / 1000000)
+            LOG.debug('{0:.2f}% complete (down: {1:.1f} kb/s up: {2:.1f} kB/s '
+                      'peers: {3:d}) {4:s} {5:d}.3'
+                      .format(status.progress * 100,
+                              status.download_rate / 1000,
+                              status.upload_rate / 1000,
+                              status.num_peers,
+                              state_str[status.state],
+                              status.total_download / 1000000))
             time.sleep(5)
-        print 'Ready for deploy'
+        LOG.debug('Ready for deploy iso {0}'.format(self.stable_iso_build))
